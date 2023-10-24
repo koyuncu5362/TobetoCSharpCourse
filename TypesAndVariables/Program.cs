90778b3b50da9352983499d247024ecf749dab7d
koyuncu5362 <koyuncu.m@hotmail.com>
2023-10-24 14:03:06 +0300

    Add project files.

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//-----------------Value Types-------------
+
+//int
+int number1 = 10;
+Console.WriteLine("Number1 is {0}", number1);
+Console.ReadLine();
+
+//long
+long number2 = 915555555555555555;
+Console.WriteLine("number2 is {0}", number2);
+Console.ReadLine();
+
+//short
+short number3 = 32767;
+Console.WriteLine("number3 is {0}", number3);
+Console.ReadLine();
+
+//byte
+byte number4 = 255;
+Console.WriteLine("number4 is {0}", number4);
+Console.ReadLine();
+
+//bool
+bool condition = true;
+Console.ReadLine();
+
+//char
+char character = 'A';
+Console.WriteLine("character is {0}", character);
+Console.ReadLine();
+
+//double
+double number5 = 255.15;
+Console.WriteLine("number5 is {0}", number5);
+Console.ReadLine();
+
+//decimal
+decimal number6 = 255.1m;
+Console.WriteLine("number6 is {0}", number6);
+Console.ReadLine();
+
+
+
+
+//var
+
+var numberrr = 10;
+var number19 = 10;
+
+Console.WriteLine("number7 is {0}", number19);
+Console.ReadLine();
