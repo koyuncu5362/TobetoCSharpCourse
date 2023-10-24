f6be6a2378a60d67aac4052f44555f68c7905907
koyuncu5362 <koyuncu.m@hotmail.com>
2023-10-24 14:08:12 +0300

    Prime Number App

---
+++ PrimeNumberApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+//---------------------------------------Prime Number-------------------------------
+static bool IsPrimeNumber(int number)
+{
+    bool result = true;
+    for (global::System.Int32 i = 2; i < number - 1; i++)
+    {
+        if (number % i == 0)
+        {
+            result = false;
+            break;
+
+        }
+    }
+    Console.WriteLine(result);
+    return result;
+}
+IsPrimeNumber(17);
