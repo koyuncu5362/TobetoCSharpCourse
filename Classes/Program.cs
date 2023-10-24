04feef9d802e16002c3b24fb53711ce4c6cc97a6
koyuncu5362 <koyuncu.m@hotmail.com>
2023-10-24 14:10:49 +0300

    Classes - Reference Types

---
+++ Classes/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//---------------------------------------Classes-------------------------------
+
+class CustomerManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Customer Added");
+    }
+    public void Update()
+    {
+        Console.WriteLine("Customer Updated");
+    }
+
+}
+CustomerManager manager = new CustomerManager();
+manager.Update();
+Console.ReadLine();
+
+//Props
+class CustomerManager2
+{
+    public int CustomerId { get; set; }
+    public void Add()
+    {
+        Console.WriteLine("Customer Added");
+    }
+    public void Update()
+    {
+        Console.WriteLine("Customer Updated");
+    }
+
+}
+CustomerManager2 manager2 = new CustomerManager2();
+manager2.CustomerId = 35;
+CustomerManager2 manager3 = new CustomerManager2()
+{
+    CustomerId = 5
+};
+manager2.Update();
+Console.ReadLine();
+
+//Encapsulation
+
+class Customer : IPerson
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Region { get { return "City " + Region; } set { Region = value; } }
+
+    public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    int IPerson.FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    int IPerson.LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
+interface IPerson
+{
+    int Id { get; set; }
+    int FirstName { get; set; }
+    int LastName { get; set; }
+
+}
---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//---------------------------------------Reference Types-------------------------------
+
+//Strings
+string city = "İstanbul";
+string city2 = "İzmir";
+var result2 = city + city2;
+Console.WriteLine(result2);
+foreach (var item in city)
+{
+    Console.WriteLine(item); ;
+
+}
+Console.ReadLine();
+
+//String Methods
+string sentence = "My name is Metin";
+Console.WriteLine(sentence.Length);
+
+var sentence2 = sentence.Clone();
+Console.WriteLine(sentence.EndsWith('n'));
+Console.WriteLine(sentence.StartsWith("My name"));
+Console.WriteLine(sentence.IndexOf("name"));
+Console.WriteLine(sentence.LastIndexOf("name"));
+Console.WriteLine(sentence.Insert(0, "hello, "));
+Console.WriteLine(sentence.Substring(3));
+Console.WriteLine(sentence.Substring(3, 5));
+Console.WriteLine(sentence.ToUpper());
+Console.WriteLine(sentence.ToLower());
+Console.WriteLine(sentence.Replace(" ", "-"));
+Console.WriteLine(sentence.Remove(2, 4));
+Console.ReadLine();
