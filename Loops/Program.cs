c4d2c191fb48069fa22459bd1b5b9ca96b1f9641
koyuncu5362 <koyuncu.m@hotmail.com>
2023-10-24 14:07:21 +0300

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//----------------------------Loops------------------------
+
+
+//For
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine(i);
+}
+Console.ReadLine();
+
+//While
+
+int number20 = 100;
+while (number20 >= 0)
+{
+    number20--;
+    Console.WriteLine(number20);
+}
+
+//Do While
+
+number20 = 100;
+do
+{
+    Console.WriteLine(number20);
+    number20--;
+} while (number20 >= 0);
+
+//ForEach
+string[] students3 = new string[3] { "Metin", "Koyuncu", "KK" };
+foreach (var student in students)
+{
+    Console.WriteLine(student);
+}
