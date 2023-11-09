07bf4ce5c9cd3bcfc52428a50b21e07d007a33ab
koyuncu5362 <koyuncu.m@hotmail.com>
2023-11-09 16:26:54 +0300

    Math

---
+++ Math/FourTransaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    public class FourTransaction
+    {
+        public void Summary(int value1,int value2)
+        {
+            int result = value1 + value2;
+            Console.WriteLine("Result is" + result);
+        }
+    }
+}
---
+++ Math/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FourTransaction four = new FourTransaction();
+            four.Summary(7, 3);
+        }
+    }
+}
