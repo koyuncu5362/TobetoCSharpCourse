8082f9fc846ee3c42e295368444e1656869909b5
koyuncu5362 <koyuncu.m@hotmail.com>
2023-11-09 16:28:47 +0300

    OOP2

---
+++ OOP2/LegalCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    public class LegalCustomer:Customer
+    {
+        public int Id { get; set; }
+        public string CustomerNo { get; set; }
+        public string TaxNo { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Customer customer1 = new Customer();
+            //customer1.FirstName = "Metin";
+            //customer1.LastName = "Koyuncu";
+            //customer1.Id= 1;
+            //customer1.TcNo = "+656546545";
+            //customer1.CustomerNo = "654654656488";
+            RealCustomer customer = new RealCustomer();
+            customer.Id= 1;
+            customer.FirstName = "aslşasg";
+            customer.CustomerNo = "37965";
+
+            LegalCustomer customer1= new LegalCustomer();
+            customer.Id = 2;
+            customer.FirstName = "aslşasg";
+            customer.CustomerNo = "37965";
+
+            Customer customer2 = new RealCustomer();
+            Customer customer3 = new LegalCustomer();
+
+            CustomerManager customerManager = new CustomerManager();    
+            customerManager.Add(customer3);
+        }
+
+    }
+}
