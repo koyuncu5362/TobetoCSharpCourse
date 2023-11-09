67212dfce1ad43994c018cab4efe94f278a8cba7
koyuncu5362 <koyuncu.m@hotmail.com>
2023-11-09 16:28:13 +0300

    OOP1

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.Name + "Added");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.Name + "Updated");
+        }
+        public int CalculateSum(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+        public void CalculateSum2(int value1, int value2)
+        {
+            Console.WriteLine(value1 + value2);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 1;
+            product.CategoryId = 1;
+            product.Name = "Test";
+            product.UnitPrice = 7;
+            product.UnitsInStock = 10;
+            Product product1 = new Product { Id = 2,CategoryId=2,Name="Test2",UnitPrice=50,UnitsInStock=7 };
+            
+            ProductManager manager = new ProductManager();
+            manager.Add()
+        }
+    }
+}
