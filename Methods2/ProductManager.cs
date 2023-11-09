c235757d52635438265835f9915717d38aa2ac14
koyuncu5362 <koyuncu.m@hotmail.com>
2023-11-09 16:27:38 +0300

    Methods 2

---
+++ Methods2/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Added" + product.Name);
+        }
+        public void AlternativeAdd(string productName,string description,double price,int unitsInStock)
+        {
+            Console.WriteLine("Added" + productName);
+        }
+    }
+}
---
+++ Methods2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Name= "Test";
+            product.UnitPrice = 15;
+            product.Description = "Description";
+
+            Product product2 = new Product();
+            product2.Name = "Test2";
+            product2.UnitPrice = 15;
+            product2.Description = "Description2";
+
+            Product[] products = new Product[] { product, product2 };
+            foreach (Product item in products)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.UnitPrice);
+                Console.WriteLine(item.Description);
+                Console.WriteLine(item.Description);
+            }
+            ProductManager manager = new ProductManager();
+            manager.Add(product2);
+
+
+            manager.AlternativeAdd("Banana", "zz", 35,5);
+            manager.AlternativeAdd("Pineapple", "zz", 35,9);
+            manager.AlternativeAdd("Strawberry", "zz", 35,7);
+        }
+    }
+}
