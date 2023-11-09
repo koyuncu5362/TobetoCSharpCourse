f1365c0acb272b11360db3e5d10784d6015315ce
koyuncu5362 <koyuncu.m@hotmail.com>
2023-11-09 16:26:18 +0300

    GenericsIntro

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           MyList<string> myList = new MyList<string>();
+            myList.Add("Selam");
+        }
+    }
+}
